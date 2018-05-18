032c8fa83fcfbf6b8ce8fcfe81bf359ce00526a2
Jeff Wong <jeffmjwong@gmail.com>
2018-05-18 11:08:06 +1000

    Promise error handling

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ navigator.getBattery().then(function(result) {
   console.log('An error occured: ' + error);
 })
 
-fetch('https://randomuser.me/api/').then(function(response) {
+fetch('https://randdomuser.me/api/').then(function(response) {
   console.log(response)
 }).catch(function(error) {
   console.log(`Something went wrong: ${error}`)
 })
 
 function readJSON(json) {
-  
+  return new Promise(function(resolve, reject) {
+    try {
+      const data = JSON.parse(json)
+      resolve(data)
+    }
+    catch(error) {
+      // Invalid JSON
+      reject({ error: true, message: error.message })
+    }
+  })
 }
+
+const x = { "name":"John", "age":"25" }
+
+readJSON(x).then(function(result) {
+  console.log(result)
+}).catch(function(err) {
+  console.log('Promise rejected: ' + err.message)
+})
